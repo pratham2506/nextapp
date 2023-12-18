a6507d08d75ba0c885a4c7977261a0e952491df8
Pratham Matkar <prathammatkar2506@gmail.com>
2023-12-18 11:07:05 +0530

    added responsive navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,26 +1,54 @@
-import React from 'react'
-import './Navbar.css'
-import Link from "next/link"
+// Navbar.js
+"use client"
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
+import './Navbar.css';
+import Link from 'next/link';
 
-const Navbar = () => {  
+const Navbar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
     <div>
-      <nav className='abcnav'>
+      <div className="drawer-toggle" onClick={toggleDrawer}>
+        <FaBars />
+      </div>
+      <nav className={`abcnav ${isDrawerOpen ? 'open' : ''}`}>
         <div className="logo">
-            <Link href='/'><img src="https://droneairgear.com/assets/Droneair-29de872f.png" alt="" /></Link>
+          <Link href='/'>
+            <img src="https://droneairgear.com/assets/Droneair-29de872f.png" alt="" />
+          </Link>
         </div>
         <ul>
-            <Link href="/components/courses" className='listt'>Courses</Link>
-            <Link href="/components/industry" className='listt'>Industry</Link>
-            <Link href="/components/internship" className='listt'>Internship</Link>
-            <Link href="/components/mentor" className='listt'>Mentor</Link>
-            <Link href="/components/university" className='listt'>University</Link>
-            <Link href="/components/drone" className='listt'>Drone</Link>
-            <Link href="/components/vcfunding" className='listt'>VC Funding</Link>
+          <Link href="/components/courses" className='listt'>
+            Courses
+          </Link>
+          <Link href="/components/industry" className='listt'>
+            Industry
+          </Link>
+          <Link href="/components/internship" className='listt'>
+            Internship
+          </Link>
+          <Link href="/components/mentor" className='listt'>
+            Mentor
+          </Link>
+          <Link href="/components/university" className='listt'>
+            University
+          </Link>
+          <Link href="/components/drone" className='listt'>
+            Drone
+          </Link>
+          <Link href="/components/vcfunding" className='listt'>
+            VC Funding
+          </Link>
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
